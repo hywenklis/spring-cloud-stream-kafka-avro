7350c2fe8643f8ccd045bec8f4e925bd2828519a
Hywenklis <hywenklis@hotmail.com>
2024-06-27 13:02:33 -0300

    Adiciona docker compose para configurar o ambiente necessário de execução dessa aplicação

---
+++ build.gradle.kts
@@ -0,0 +1,49 @@
+plugins {
+	id("org.springframework.boot") version "3.3.1"
+	id("io.spring.dependency-management") version "1.1.5"
+	kotlin("jvm") version "1.9.24"
+	kotlin("plugin.spring") version "1.9.24"
+}
+
+group = "com.hywenklis"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(21)
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+extra["springCloudVersion"] = "2023.0.2"
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("org.springframework.cloud:spring-cloud-stream")
+	developmentOnly("org.springframework.boot:spring-boot-docker-compose")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+	testImplementation("org.springframework.cloud:spring-cloud-stream-test-binder")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+dependencyManagement {
+	imports {
+		mavenBom("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
+	}
+}
+
+kotlin {
+	compilerOptions {
+		freeCompilerArgs.addAll("-Xjsr305=strict")
+	}
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
---
+++ src/test/kotlin/com/hywenklis/spring/cloud/stream/kafka/avro/SpringCloudStreamKafkaAvroApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.hywenklis.spring.cloud.stream.kafka.avro
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringCloudStreamKafkaAvroApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
