5706bb213f402a9d7380bd23665d906356ed5ed8
Hywenklis <hywenklis@hotmail.com>
2024-06-27 17:06:57 -0300

    Adiciona configuração de comunicação com kafka

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
 	id("io.spring.dependency-management") version "1.1.5"
 	kotlin("jvm") version "1.9.24"
 	kotlin("plugin.spring") version "1.9.24"
+	id("com.github.davidmc24.gradle.plugin.avro") version "1.9.1"
 }
 
 group = "com.hywenklis"
@@ -19,16 +20,25 @@ repositories {
 }
 
 extra["springCloudVersion"] = "2023.0.2"
+extra["h2DatabaseVersion"] = "2.2.224"
+extra["apacheAvroVersion"] = "1.11.3"
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.springframework.cloud:spring-cloud-stream")
+	implementation("org.springframework.cloud:spring-cloud-stream-binder-kafka")
+	implementation("org.apache.avro:avro:${property("apacheAvroVersion")}")
+
+	runtimeOnly("com.h2database:h2:${property("h2DatabaseVersion")}")
+
 	developmentOnly("org.springframework.boot:spring-boot-docker-compose")
+
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 	testImplementation("org.springframework.cloud:spring-cloud-stream-test-binder")
+
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
@@ -46,4 +56,12 @@ kotlin {
 
 tasks.withType<Test> {
 	useJUnitPlatform()
+}
+
+sourceSets {
+	main {
+		java {
+			srcDirs("src/main/kotlin", "build/generated-main-avro-java")
+		}
+	}
 }
